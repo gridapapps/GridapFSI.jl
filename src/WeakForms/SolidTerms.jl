164a76ad1cf5af2aaa7f61a12b5b25a24f555a4c
Oriol Colomes <oriol.colomes@gmail.com>
2020-08-22 14:37:22 +0200

    Removed pressure variable from solid terms

--- src/WeakForms/SolidTerms.jl
+++ src/WeakForms/SolidTerms.jl
@@ -1,42 +1,42 @@
 function a_FSI_ϕ_Ωs(x, xt, y)
-  u,v,p = x
-  ut, vt,pt = xt
-  ϕ,φ,q = y
+  u,v = x
+  ut, vt = xt
+  ϕ,φ = y
   (ϕ⋅ut) - (ϕ⋅v)
 end
 function l_FSI_ϕ_Ωs(y,f,t)
-  ϕ,φ,q = y
+  ϕ,φ = y
   (ϕ⋅f(t))
 end
 function a_FSI_φ_Ωs(x, xt, y, ρ, Es, νs)
-  u,v,p = x
-  ut, vt,pt = xt
-  ϕ,φ,q = y
+  u,v = x
+  ut, vt = xt
+  ϕ,φ = y
   (λ,μ) = lame_parameters(Es,νs)
   (φ⋅(ρ*vt)) + (∇(φ) ⊙ Ps(λ,μ,u))
 end
 function l_FSI_φ_Ωs(y,f,t)
-  ϕ,φ,q = y
+  ϕ,φ = y
   (φ⋅f(t))
 end
 
 
 function da_FSI_dx_ϕ_Ωs(x, dx, y)
-  u,v,p = x
-  du,dv,dp = dx
-  ϕ,φ,q = y
+  u,v = x
+  du,dv = dx
+  ϕ,φ = y
   0.0*(du⋅ϕ) - (ϕ⋅dv)
 end
 function da_FSI_dx_φ_Ωs(x, dx, y, ρ, E, ν)
-  u,v,p = x
-  du,dv,dp = dx
-  ϕ,φ,q = y
+  u,v = x
+  du,dv = dx
+  ϕ,φ = y
   (λ,μ) = lame_parameters(E,ν)
   0.0*(φ⋅(ρ*dv)) + (∇(φ) ⊙ ( dF(∇(du))⋅S_SV(∇(u),λ,μ) ) ) + ( ∇(φ) ⊙ (F(∇(u))⋅dS_SV(∇(u),∇(du),λ,μ)) )
 end
 function da_FSI_dxt_Ωs(x, dxt, y, ρ)
-  u, v, p = x
-  dut, dvt, dpt = dxt
-  ϕ, φ, q = y
+  u, v = x
+  dut, dvt = dxt
+  ϕ, φ = y
   ϕ⋅dut + (φ⋅(ρ*dvt))
 end
